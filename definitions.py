7306a301bb7ac4fdf5bfacb82c1c5ddc6c95a6dc
Riley Bakes <riley.o.bakes@gmail.com>
2020-02-14 13:21:08 -0500

    raw string bug

--- definitions.py
+++ definitions.py
@@ -2,4 +2,4 @@ import os
 import datetime
 # dir_results = os.path.join(ROOT_DIR, "results", "encoder_decoder_lstm_001.txt")
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-LOGDIR = os.path.join(ROOT_DIR, r"logs\scalars")
+LOGDIR = os.path.join(ROOT_DIR, "logs","scalars")
